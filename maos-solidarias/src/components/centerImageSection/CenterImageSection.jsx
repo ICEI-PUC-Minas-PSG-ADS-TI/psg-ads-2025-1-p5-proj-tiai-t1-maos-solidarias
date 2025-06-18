756487b3585107a75cc704ec9b3033825544557d
Bielzitcho <gabriel.m.freeitas@gmail.com>
2025-06-17 23:15:14 -0300

    adicionando redirecionamento no botão da imagem

--- maos-solidarias/src/components/centerImageSection/CenterImageSection.jsx
+++ maos-solidarias/src/components/centerImageSection/CenterImageSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './centerImageSection.module.css';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import Link from 'next/link';
 
 const CenterImageSection = ({ title, description, showButton = true, image, align, mh = '50vh' }) => {
   return (
@@ -8,7 +9,7 @@ const CenterImageSection = ({ title, description, showButton = true, image, alig
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.description}>{description}</p>
       <div className={styles.buttonContainer}>
-        {showButton && <button className={styles.button}><MailOutlineIcon />Registrar como ONG</button>}
+        {showButton && <Link href={'/register'}><button className={styles.button}><MailOutlineIcon />Registrar como ONG</button></Link>}
       </div>
     </div>
   );
